fb7b5be077c2cd93402fa8410eb92264c66dc38f
johnsonandson <john.speer042@gmail.com>
2025-11-26 18:07:08 -0500

    Improve canFlowOff method with bounds checking
    Refactor canFlowOff method to include bounds checking for row and column indices.

--- Main.java
+++ Main.java
@@ -1,55 +1,43 @@
     
     public class Main{
-    //     public static <T extends Comparable<T>>  
-
-    //     boolean binarySearch(T[] data, int min, int max, T target){
-    //         if (min>(max)){
-    //             return false;
-    //         }
-    //         if (target.equals(data[(max+min)/2])){
-    //             return true;
-    //         }
-    //         else if(target.compareTo(data[(max+min)/2])<0){
-    //             return (binarySearch(data, min, (max+min)/2-1,target));
-    //         }else{
-    //             return (binarySearch(data, (min+max)/2+1,max,target));
-    //         }
-
-    // }
-    public static boolean canFlowOff(int[][] map,int row, int col){
-        boolean flow=false;
-        if (row+1>map.length-1||row-1<0||col+1>map[0].length-1||col-1<0){
-            return true;
-        }
-        if (map[row][col]>map[row][col+1]){
-            flow= canFlowOff(map,row,col+1);
-        }
-
-        if (flow){
-            return true;
-        }
-        if (map[row][col]>map[row][col-1]){
-            flow= canFlowOff(map,row,col-1);
-        }
-
-        if (flow){
-            return true;
-        }
-        if (map[row][col]>map[row+1][col]){
-            flow= canFlowOff(map,row+1,col);
-        }
-
-        if (flow){
-            return true;
-        }
-        if (map[row][col]>map[row-1][col]){
-            flow= canFlowOff(map,row-1,col);
-        }
-        if (flow){
-            return true;
-        }
-        return false;
-    }
+	
+	    public static boolean canFlowOff(int[][] map,int row, int col){
+			if (map.length<=row||map[0].length<=col||row<0||col<0){
+				return false;
+			}
+	        boolean flow=false;
+	        if (row+1>map.length-1||row-1<0||col+1>map[0].length-1||col-1<0){
+	            return true;
+	        }
+	        if (map[row][col]>map[row][col+1]){
+	            flow= canFlowOff(map,row,col+1);
+	        }
+	
+	        if (flow){
+	            return true;
+	        }
+	        if (map[row][col]>map[row][col-1]){
+	            flow= canFlowOff(map,row,col-1);
+	        }
+	
+	        if (flow){
+	            return true;
+	        }
+	        if (map[row][col]>map[row+1][col]){
+	            flow= canFlowOff(map,row+1,col);
+	        }
+	
+	        if (flow){
+	            return true;
+	        }
+	        if (map[row][col]>map[row-1][col]){
+	            flow= canFlowOff(map,row-1,col);
+	        }
+	        if (flow){
+	            return true;
+	        }
+	        return false;
+	    }
         public static void main(String[] args) {
             int[][] map = {
 			{100, 99, 200, 200, 200, 200, 200, 200, 200, 200}, 
@@ -64,6 +52,37 @@
 			{200, 98, 200, 200, 200, 200, 200, 200, 200, 76}};
             
             if (canFlowOff(map,1,3)){
+                System.out.println("yep");
+            }
+            else{
+                System.out.println("nah");
+            }
+			if (canFlowOff(map,11,3)){
+                System.out.println("yep");
+            }
+            else{
+                System.out.println("nah");
+            }
+			if (canFlowOff(map,1,3)){
+                System.out.println("yep");
+            }
+            else{
+                System.out.println("nah");
+            }
+			map = {
+			{100, 200, 200, 200, 200, 200, 200, 200, 200, 200}, 
+			{200, 200, 200, 200, 200, 200, 200, 200, 200, 200},
+			{200, 10, 200, 200, 200, 200, 200, 200, 200, 200},
+			{200, 11, 10, 200, 200, 6, 85, 84, 83, 200},
+			{200, 200, 14, 15, 59, 200, 86, 200, 82, 200},
+			{200, 11, 12, 200, 200, 200, 87, 200, 81, 200},
+			{200, 10, 200, 200, 90, 89, 88, 200, 200, 200},
+			{200, 9, 8, 200, 200, 200, 200, 200, 200, 200},
+			{200, 200, 7, 200, 200, 200, 200, 200, 200, 200},
+			{200, 98, 6, 200, 200, 200, 200, 200, 200, 200}
+			};
+
+            if (canFlowOff(map,4,5)){
                 System.out.println("yep");
             }
             else{
